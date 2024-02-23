260f9fbb4a31fc21228645f991cd6379a7c4508f
Pedro Tomasini <pedrotomasini1@outlook.com>
2024-02-23 19:02:11 -0300

    Recuperando informação

--- integrationWithMongo/secondFilePymongo.py
+++ integrationWithMongo/secondFilePymongo.py
@@ -1,6 +1,17 @@
+import pprint
+
 import pymongo as pyM
 
 client = pyM.MongoClient("mongodb+srv://pedrotomasini1:8VLT0GKPoDsZBAIB@cluster0.vmfziwd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 db = client.test
 posts = db.posts
+
+for post in posts.find():
+    pprint.pprint(post)
+
+print(posts.count_documents({}))
+print(posts.count_documents({"author": "John"}))
+print(posts.count_documents({"tags": "python"}))
+
+pprint.pprint(posts.find_one({"author": "John"}))
