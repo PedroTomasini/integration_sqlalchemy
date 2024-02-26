8dfa45f9382b346f3569eab38ee9503dbe0ac4bc
Pedro Tomasini <pedrotomasini1@outlook.com>
2024-02-26 18:15:10 -0300

    Buscando documentos 2

--- integrationWithMongo/secondFilePymongo.py
+++ integrationWithMongo/secondFilePymongo.py
@@ -1,5 +1,6 @@
 import pprint
 
+import pymongo
 import pymongo as pyM
 
 client = pyM.MongoClient("mongodb+srv://pedrotomasini1:8VLT0GKPoDsZBAIB@cluster0.vmfziwd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
@@ -15,3 +16,20 @@ print(posts.count_documents({"author": "John"}))
 print(posts.count_documents({"tags": "python"}))
 
 pprint.pprint(posts.find_one({"author": "John"}))
+
+print("\nRecuperando documentos na coleção posts de maneira ordenada por data.")
+for post in posts.find({}).sort("date"):
+    pprint.pprint(post)
+
+result = db.profiles.create_index([("author", pymongo.ASCENDING)], unique=True)
+print(sorted(list(db.profiles.index_information())))
+
+user_profile_user = [
+    {'user_id': 1, 'name': 'John'},
+    {'user_id': 2, 'name': 'Jane'},]
+
+result = db.profile.insert_many(user_profile_user)
+print("\nColeções armazenadas no MongoDB:")
+collections = db.list_collection_names()
+for collection in collections:
+    print(collection)
